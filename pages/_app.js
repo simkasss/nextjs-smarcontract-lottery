6f500dbc68c92bc4f7912f690bea049b32a4754f
simkasss <s.kastantinaviciute@gmail.com>
2023-01-28 14:02:34 +0200

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-import '@/styles/globals.css'
+import { MoralisProvider } from "react-moralis"
+import { NotificationProvider } from "web3uikit"
+import "../styles/globals.css"
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+    return (
+        <MoralisProvider initializeOnMount={false}>
+            <NotificationProvider>
+                <Component {...pageProps} />
+            </NotificationProvider>
+        </MoralisProvider>
+    )
 }
+
+export default MyApp
